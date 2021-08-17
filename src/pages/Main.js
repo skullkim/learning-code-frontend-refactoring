a6ee9ee4e2d545822108f08f227f123d5d3dcd52
skullKim <kyk990328@gmail.com>
2021-08-17 12:33:31 +0900

    추가: cors 테스트

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,13 @@
+import axios from 'axios';
+import {useEffect} from 'react';
+
+/* eslint-disable */
 const Main = () => {
+  useEffect(() => {
+    axios.get('http://localhost:8080/main-page-images')
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  });
   return (<div>main</div>);
 }
 

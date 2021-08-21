0e4720b255ea6db261d2a014e97caea1d0e24b26
skullKim <kyk990328@gmail.com>
2021-08-21 18:08:10 +0900

    추가: 도서 검색 완료

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -32,6 +32,48 @@ const LetterCategory = styled.p`
     margin-bottom: 10px;
 `;
 
+const BookBox = styled.section`
+  height: 150px;
+  width: 100%;
+  margin-bottom: 10px;
+  border-bottom: 1px solid gray;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
+
+const BookInfo = styled.section`
+  height: 100px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin-left: 10px;
+`;
+
+const BookTitle = styled.h4`
+  margin: 0;
+`;
+
+const BookImage = styled.img`
+  height: 100px;
+  width: 70px;
+`;
+
+const BookIntroduction = styled.div`
+  height: 100px;
+  width: 100%;
+  margin-left: 10px;
+`;
+
+const BookAuthors = styled.h5`
+  margin-top: 0;
+  margin-bottom: 5px;
+`;
+
+const BookContents = styled.p`
+    margin: 0;
+`;
+
 const SearchResult = ({search}) => {
     const [searchResult, setSearchResult] = useState([]);
     const [noResult, setNoResult] = useState('');
@@ -50,8 +92,8 @@ const SearchResult = ({search}) => {
         setSearchResult([]);
         axios.get(`${process.env.REACT_APP_SERVER_ORIGIN}/search/${category}?query=${query}`)
             .then(({data: {data}}) => {
-                setNoResult(() => data[0].message ?? '');
-                setSearchResult(() => data);
+                setNoResult(data[0].message ?? '');
+                setSearchResult(data);
             })
             .catch(err => err);
         setLoading(false);
@@ -65,13 +107,27 @@ const SearchResult = ({search}) => {
         <SearchPageBox>
             <Category />
             <ResultBox>
-                {searchResult && !noResult && searchResult.map(({id, title, main_category: mainCategory}) => (
-                    <LetterBox key={id}>
-                        <LetterTitle>{title}</LetterTitle>
-                        <LetterCategory>{mainCategory}</LetterCategory>
-                    </LetterBox>
-                ))}
                 {searchResult && noResult && <LetterTitle>{query}에 대한 검색 결과가 없습니다</LetterTitle>}
+                {category !== 'book' ?
+                    searchResult && !noResult && searchResult.map(({ id, title, main_category: mainCategory }) => (
+                        <LetterBox key={id}>
+                            <LetterTitle>{title}</LetterTitle>
+                            <LetterCategory>{mainCategory}</LetterCategory>
+                        </LetterBox>
+                    )) :
+                    searchResult && searchResult.map(({authors, contents, title, thumbnail, url}) => (
+                        <BookBox key={thumbnail}>
+                            <BookTitle><a href={url}>{title}</a></BookTitle>
+                            <BookInfo>
+                                <BookImage src={thumbnail} alt={`${title} thumbnail`}/>
+                                <BookIntroduction>
+                                    <BookAuthors>{authors.map(author => `${author}, `)}</BookAuthors>
+                                    <BookContents>{contents}</BookContents>
+                                </BookIntroduction>
+                            </BookInfo>
+                        </BookBox>
+                    ))
+                }
             </ResultBox>
         </SearchPageBox>
     );

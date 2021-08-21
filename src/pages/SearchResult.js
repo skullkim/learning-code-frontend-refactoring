c8767979c6df2b626e5d99fa0ec56f7253263fdc
skullKim <kyk990328@gmail.com>
2021-08-21 17:03:41 +0900

    추가: 카테고리, 타이틀 검색 완료

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -1,32 +1,79 @@
-/* eslint-disable */
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import qs from 'qs';
 import {useEffect, useState} from "react";
 import {useLocation, useParams} from 'react-router-dom';
+import styled from 'styled-components';
 
 import Category from "../components/Category";
 
+const SearchPageBox = styled.main`
+  width: 100%;
+  height: auto;
+  display: flex;
+`;
+
+const ResultBox = styled.article`
+  width: 100%;
+  height: 100%;
+  margin: 0 30px;
+`;
+
+const LetterBox = styled.section`
+  height: 70px;
+  width: 100%;
+  margin-bottom: 10px;
+  border-bottom: 1px solid gray;
+`;
+
+const LetterTitle = styled.h4``;
+
+const LetterCategory = styled.p`
+    margin-bottom: 10px;
+`;
+
 const SearchResult = ({search}) => {
+    const [searchResult, setSearchResult] = useState([]);
+    const [noResult, setNoResult] = useState('');
+    const [loading, setLoading] = useState(false);
     const {query} = qs.parse(useLocation().search, {
         ignoreQueryPrefix: true,
     });
     const {category} = useParams();
 
     useEffect(() => {
-      search(false);
+        search(false);
     });
 
     useEffect(() => {
+        setLoading(true);
+        setSearchResult([]);
         axios.get(`${process.env.REACT_APP_SERVER_ORIGIN}/search/${category}?query=${query}`)
-            .then(({data: {data}}) => console.log(data))
-            .catch(err => console.log(err));
-    }, []);
+            .then(({data: {data}}) => {
+                setNoResult(() => data[0].message ?? '');
+                setSearchResult(() => data);
+            })
+            .catch(err => err);
+        setLoading(false);
+    }, [category, query]);
+
+    if(loading) {
+        return <div>loading...</div>;
+    }
 
     return (
-      <div>
-          <Category />
-      </div>
+        <SearchPageBox>
+            <Category />
+            <ResultBox>
+                {searchResult && !noResult && searchResult.map(({id, title, main_category: mainCategory}) => (
+                    <LetterBox key={id}>
+                        <LetterTitle>{title}</LetterTitle>
+                        <LetterCategory>{mainCategory}</LetterCategory>
+                    </LetterBox>
+                ))}
+                {searchResult && noResult && <LetterTitle>{query}에 대한 검색 결과가 없습니다</LetterTitle>}
+            </ResultBox>
+        </SearchPageBox>
     );
 }
 

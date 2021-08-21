d4af69a8525a7eed7f05fe6595e03df551238f11
skullKim <kyk990328@gmail.com>
2021-08-21 15:08:03 +0900

    추가: 카테고리 부분 골자 완료

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,5 +1,30 @@
+import styled from 'styled-components';
+
+const CategoryBox = styled.aside`
+  height: 900px;
+  width: 200px;
+  background-color: red;
+`;
+
+const MainCategory = styled.dt`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const SubCategory = styled.dd`
+  font-size: 15px;
+  font-weight: normal;
+`;
+
 const Category = () => {
-    return <div>category</div>
+    return (
+        <>
+            <CategoryBox>
+                <MainCategory>Main</MainCategory>
+                <SubCategory>Sub</SubCategory>
+            </CategoryBox>
+        </>
+    );
 }
 
 export default Category;

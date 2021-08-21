5d9eed87e1f360f69d40b9bb0ed29e0779ae4994
skullKim <kyk990328@gmail.com>
2021-08-21 13:35:10 +0900

    추가: 푸터 골자 완료

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,50 @@
+import styled from 'styled-components';
+
+const FooterBox = styled.footer`
+    height: 80px;
+    width: 100%;;
+    background-color: red;
+    display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+    height: 60px;
+    width: 80px;
+    background-color: #61dafb;
+`;
+
+const InfoBox = styled.article`
+  height: 60px;
+  width: 200px;
+  background-color: yellow;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Developer = styled.h4`
+  margin: 0;
+`;
+
+const GithubLink = styled.a`
+  
+`;
+
 const Footer = () => {
-    return (<div>footer</div>)
+    return (
+        <>
+            <FooterBox>
+                <Logo />
+                <InfoBox>
+                  <Developer>Made by skullKim</Developer>
+                  <GithubLink href="https://github.com/skullkim">github link</GithubLink>
+                </InfoBox>
+            </FooterBox>
+        </>
+    );
 }
 
 export default Footer;

c631202a2b218d9ceddf2ef957aeb8783d987da5
skullKim <kyk990328@gmail.com>
2021-08-21 14:15:03 +0900

    추가: 메인 페이지 골자 완료

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,14 +1,42 @@
 import axios from 'axios';
-import {useEffect} from 'react';
+import {useState, useEffect} from 'react';
+import styled from "styled-components";
+
+const MainBox = styled.main`
+  height: 900px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: red;
+`;
+
+const ImageBox = styled.div`
+  height: 700px;
+  width: 80%;
+  background-color: yellow;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+
 
 /* eslint-disable */
 const Main = () => {
+  const [imageUrls, setImageUrls] = useState([]);
   useEffect(() => {
     axios.get('http://localhost:8080/main-page-images')
-      .then(data => console.log(data))
-      .catch(err => console.log(err));
-  });
-  return (<div>main</div>);
+      .then(({data: {data}}) => setImageUrls(data))
+      .catch(err => err);
+  }, []);
+  console.log(imageUrls);
+  return (
+      <>
+          <MainBox>
+              <ImageBox />
+          </MainBox>
+      </>
+  );
 }
 
 export default Main;

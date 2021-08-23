b3a6f691ce7c0ee47547a42e23ceed7b67915763
skullKim <kyk990328@gmail.com>
2021-08-23 20:52:52 +0900

    추가: 로컬 로그인 골자 완료

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,5 +1,57 @@
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+const LoginBox = styled.article`
+  width: 100%;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LocalLoginBox = styled.section`
+  width: 35%;
+  height: 300px;
+`;
+
+const LoginTitle = styled.h4`
+    text-align: center;
+`;
+
+const LoginInput = styled.input`
+  width: 100%;
+  height: 20px;
+  margin-top: 10px;
+`;
+
+const LoginBtnBox = styled.div`
+  width: 103%;
+  height: 20px;
+  margin-top: 15px;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const LoginBtn = styled.button`
+  width: 49%;
+  height: 20px;
+`;
+
+
 const Signin = () => {
-    return (<div>sign in</div>);
+    return (
+        <LoginBox>
+            <LocalLoginBox>
+                <LoginTitle>Learning Code 로그인</LoginTitle>
+                <LoginInput placeholder='email'/>
+                <LoginInput type='password' placeholder='password' />
+                <LoginBtnBox>
+                    <LoginBtn>Sign in</LoginBtn>
+                    <LoginBtn><Link to='/signin/password'>Find password</Link></LoginBtn>
+                </LoginBtnBox>
+            </LocalLoginBox>
+        </LoginBox>
+    );
 }
 
 export default Signin;

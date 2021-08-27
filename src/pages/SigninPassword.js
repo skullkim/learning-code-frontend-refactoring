1180710aa23e2adba30d9d31e6da020549b03972
skullKim <kyk990328@gmail.com>
2021-08-27 15:39:50 +0900

    추가: 비밀번호 변경 페이지 디자인 완료

--- src/pages/SigninPassword.js
+++ src/pages/SigninPassword.js
@@ -1,5 +1,38 @@
+import styled from 'styled-components';
+
+import Auth, {AuthTitle, AuthInput} from '../components/Auth';
+
+const SubmitBtn = styled.button`
+  width: 51%;
+  height: 30px;
+  margin-top: 15px;
+`;
+
 const SigninPassword = () => {
-    return (<div>signin password</div>);
+    return (
+        <Auth>
+            <>
+                <AuthTitle>비밀번호 찾기</AuthTitle>
+                <AuthInput
+                    type='text'
+                    name='email'
+                    placeholder='이메일'
+                />
+                <AuthInput
+                    type='password'
+                    name='password'
+                    placeholder='비밀번호'
+                />
+                <AuthInput
+                    type='password'
+                    name='verifyPasswd'
+                    placeholder='비밀번호 확인'
+                />
+                <SubmitBtn type='submit'>비밀번호 변경</SubmitBtn>
+            </>
+        </Auth>
+
+    )
 }
 
 export default SigninPassword;

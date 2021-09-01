7a4a4365d52941961214b9fb0504ed6174de6aa3
skullKim <kyk990328@gmail.com>
2021-09-01 21:34:06 +0900

    수정: 엑세스 토큰 만료로 리프레시 토큰으로 인증 요청 후 로컬스토리지 삭제 안되는 문제 해결

--- src/lib/reissuanceToken.js
+++ src/lib/reissuanceToken.js
@@ -1,31 +1,3 @@
-// import axios from 'axios';
-//
-// /*eslint-disable*/
-// import { saveUserInfo } from "../modules/auth";
-//
-// const reissuingToken = () => {
-//     const {userId} = JSON.parse(
-//         localStorage.getItem('userInfo')
-//     );
-//     axios({
-//         method: 'post',
-//         url: `${process.env.REACT_APP_SERVER_ORIGIN}/authentication/token`,
-//         withCredentials: true,
-//     })
-//         .then(({data: {data: {access_token: accessToken}}}) => {
-//             localStorage.setItem('userInfo', JSON.stringify({
-//                 userId: `${userId}`,
-//                 accessToken,
-//             }));
-//             saveUserInfo({userId: `${userId}`, accessToken});
-//         })
-//         .catch(error => {
-//             console.log('aaa', error);
-//         });
-// }
-//
-// export default reissuingToken;
-
 import axios from 'axios';
 
 /*eslint-disable*/

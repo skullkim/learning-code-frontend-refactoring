268b334303ed900ca6bcb0cd0cad8217e5f4631d
skullKim <kyk990328@gmail.com>
2021-09-04 18:22:11 +0900

    추가: 기존 유저 정보 불러오기 완료

--- src/pages/UserInfo.js
+++ src/pages/UserInfo.js
@@ -1,6 +1,9 @@
+import {useEffect, useState} from 'react';
 import styled from 'styled-components';
 
 import Auth, {AuthInput} from '../components/Auth';
+import Api from '../lib/customAxios';
+import getUserInfo from '../lib/getUserInfo';
 
 const ProfileImgLabel = styled.label`
   display: flex;
@@ -14,7 +17,32 @@ const ChangeProfileBtn = styled.button`
   margin-top: 15px
 `;
 
+/*eslint-disable*/
 const UserInfo = () => {
+    const [userProfile, setUserProfile] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [userInfo] = useState(getUserInfo());
+
+    useEffect(() => {
+        setLoading(true);
+        Api({
+            method: 'get',
+            url: `${process.env.REACT_APP_SERVER_ORIGIN}/user/${userInfo.userId}`,
+            headers: {
+                'Authorization': `Bearer ${userInfo.accessToken}`
+            }
+        })
+            .then(({data: {data}}) => {
+                setUserProfile(data);
+                setLoading(false);
+            })
+            .catch(err => err);
+    }, []);
+
+    if(loading) {
+        return <div>loading...</div>
+    }
+
     return (
         <Auth>
             <>
@@ -26,8 +54,16 @@ const UserInfo = () => {
                         id='profile-img'
                     />
                 </ProfileImgLabel>
-                <AuthInput type='text' name='name'/>
-                <AuthInput type='text' name='email'/>
+                <AuthInput
+                    type='text'
+                    name='name'
+                    placeholder= {`${userProfile.name || 'name'}`}
+                />
+                <AuthInput
+                    type='text'
+                    name='email'
+                    placeholder={`${userProfile.email || 'email'}`}
+                />
                 <ChangeProfileBtn>Change profile</ChangeProfileBtn>
             </>
         </Auth>

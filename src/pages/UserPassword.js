56cd3b7d10cbc8897ee7f699b099e264991182fe
skullKim <kyk990328@gmail.com>
2021-09-05 19:30:09 +0900

    비밀 번호 수정 페이지 디자인 완료

--- src/pages/UserPassword.js
+++ src/pages/UserPassword.js
@@ -1,5 +1,37 @@
+import styled from 'styled-components';
+
+import Auth, {AuthTitle, AuthInput} from '../components/Auth';
+
+const SubmitBtn = styled.button`
+  width: 52%;
+  height: 30px;
+  margin-top: 15px;
+`;
+
 const UserPassword = () => {
-    return (<div>edit password</div>);
+    return (
+        <Auth>
+            <>
+                <AuthTitle>비밀번호 변경</AuthTitle>
+                <AuthInput
+                    type='password'
+                    name='prevPassword'
+                    placeholder='이전 비밀번호'
+                />
+                <AuthInput
+                    type='password'
+                    name='newPassword'
+                    placeholder='새로운 비밀번호'
+                />
+                <AuthInput
+                    type='password'
+                    name='verifyPassword'
+                    placeholder='비밃번호 확인'
+                />
+                <SubmitBtn>비밀번호 변경</SubmitBtn>
+            </>
+        </Auth>
+    );
 };
 
 export default UserPassword;
